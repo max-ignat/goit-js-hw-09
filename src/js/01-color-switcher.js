0be80252bec7ecf4f139fca8f5e457be1a33ce9e
max-ignat <cadillac.maxim@gmail.com>
2022-11-19 17:08:10 +0200

    summary

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
-ewsfd
+import '../css/common.css';
+
+const body = document.body;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function startChange(event) {
+  timerId = setInterval(changeBodyColor, 1000);
+
+  console.log('started');
+}
+
+function stopChange(event) {
+  clearInterval(timerId);
+  timerId = 0;
+  console.log('stopped');
+}
+
+startBtn.addEventListener('click', startChange, { once: true });
+stopBtn.addEventListener('click', stopChange);
