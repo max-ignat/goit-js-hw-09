7c31def22a11d12ec421ce611c8bffc7a76045f0
max-ignat <cadillac.maxim@gmail.com>
2022-11-24 14:30:44 +0200

    summary

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const elems = evt.currentTarget.elements;
+
+  let delayVal = Number(elems.delay.value);
+  const stepVal = Number(elems.step.value);
+  let amountVal = Number(elems.amount.value);
+
+  for (let position = 1; position <= amountVal; position++) {
+    if (position !== 1) {
+      delayVal += stepVal;
+    }
+    createPromise(position, delayVal)
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
   }
 }
+
+formEl.addEventListener('submit', onFormSubmit);
